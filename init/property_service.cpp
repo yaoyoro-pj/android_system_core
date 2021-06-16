b25232afcf326d8ca39779ad3b845cfc7da0d254
Jarl-Penguin <jarlpenguin@outlook.com>
2021-06-16 11:28:46 +0000

    init: Don't spoof SafetyNet properties in recovery mode
    Change-Id: Ib6d3808c3b8f3e0cffab685a24d3cdd436b0fe9b

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1416,8 +1416,13 @@ void PropertyInit() {
         LOG(FATAL) << "Failed to load serialized property info file";
     }
 
-    // Report valid verified boot chain to help pass Google SafetyNet integrity checks
-    SetSafetyNetProps();
+    // Report a valid verified boot chain to make Google SafetyNet integrity
+    // checks pass. This needs to be done before parsing the kernel cmdline as
+    // these properties are read-only and will be set to invalid values with
+    // androidboot cmdline arguments.
+    if (!IsRecoveryMode()) {
+      SetSafetyNetProps();
+    }
 
     // If arguments are passed both on the command line and in DT,
     // properties set in DT always have priority over the command-line ones.
