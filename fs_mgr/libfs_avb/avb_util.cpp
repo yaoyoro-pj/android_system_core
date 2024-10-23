92f4a77250f76a0931a30975f43c586d517feb14
Juhyung Park <qkrwngud825@gmail.com>
2024-10-23 12:15:10 +0800

    fs_mgr: Always use "check_at_most_once"
    dm-verity's overhead is non-negligible.
    
    As we're optimizing for performance over security, always use
    "check_at_most_once".
    
    This feature is available on kernels v4.17 and onwards and is
    equivalent to CONFIG_DM_ANDROID_VERITY_AT_MOST_ONCE_DEFAULT_ENABLED
    on older kernels.
    
    Change-Id: Ib8b13d8429ac62bb1553ae81a7175d33fdb2e471
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- fs_mgr/libfs_avb/avb_util.cpp
+++ fs_mgr/libfs_avb/avb_util.cpp
@@ -79,9 +79,7 @@ bool ConstructVerityTable(const FsAvbHashtreeDescriptor& hashtree_desc,
     // Always use ignore_zero_blocks.
     target.IgnoreZeroBlocks();
 
-    if (hashtree_desc.flags & AVB_HASHTREE_DESCRIPTOR_FLAGS_CHECK_AT_MOST_ONCE) {
         target.CheckAtMostOnce();
-    }
 
     LINFO << "Built verity table: '" << target.GetParameterString() << "'";
 
